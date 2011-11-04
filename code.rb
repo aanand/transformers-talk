b0e2e3188659eaa8e1b993c9d2689ed8ff52a853
Aanand Prasad <aanand.prasad@gmail.com>
2011-11-04 10:41:47 +0000

    Continuation examples

--- code.rb
+++ code.rb
@@ -385,5 +385,77 @@ module Examples
       row.id
     end
   end
+
+  def goliath_example
+    require 'goliath'
+    require 'em-synchrony/em-http'
+
+    class GoogleProxy < Goliath::API
+      def response(env)
+        http = EM::HttpRequest.new("http://google.com").get
+        [200, {'X-Goliath' => 'Proxy','Content-Type' => 'text/html'}, http.response]
+      end
+    end
+  end
+
+  def sequence_example_callcc
+    Sequence.run do |o|
+      x =o< 1
+      y =o< 2
+
+      x + y
+    end
+  end
+
+  def nil_check_example_callcc
+    NilCheck.run do |o|
+      x =o< 1
+      y =o< nil
+
+      x + y
+    end
+  end
+
+  def search_example_callcc
+    Search.run do |o|
+      x =o< ["first", "second"]
+      y =o< ["once", "twice"]
+
+      ["#{x} cousin #{y} removed"]
+    end
+  end
+
+  def transformer_definition_callcc
+    require 'continuation'
+
+    module Transformer
+      def run(&fn)
+        callcc do |the_end|
+          stack = [the_end]
+
+          next_continuation = proc do |arg|
+            stack.pop.call(arg)
+          end
+
+          o = proc do |mval|
+            callcc do |rtn|
+              res = bind(mval) do |value|
+                callcc do |cc|
+                  stack.push(cc)
+                  rtn.call(value)
+                end
+              end
+
+              next_continuation.call(res)
+            end
+          end
+
+          (class << o; self; end).class_eval { alias_method :<, :call }
+
+          next_continuation.call(instance_exec(o, &fn))
+        end
+      end
+    end
+  end
 end
 
