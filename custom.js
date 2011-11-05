02343a38e093ed421ce5b090801c59f5a0248514
Aanand Prasad <aanand.prasad@gmail.com>
2011-11-05 12:04:15 +0000

    Fixes to highlighting
    - don't attempt to highlight blocks without a language class
    - remove console.log
    - hack to fix Ruby 1.9 hash syntax

--- custom.js
+++ custom.js
@@ -1,9 +1,16 @@
 // Intercept call to sh_highlightDocument
 function sh_highlightDocument() {
   $('pre > code').each(function(i, e) {
+    if (!this.parentNode.className) return;
     var language = this.parentNode.className.match(/\bsh_(\w+)\b/)[1];
-    console.log(language);
     this.innerHTML = hljs.highlight(language, $(this).text()).value;
+
+    // Fix highlighting of Ruby 1.9 symbol hash key style
+    $('.symbol', this).each(function(i, e) {
+      if (e.previousSibling.className === 'identifier') {
+        e.previousSibling.className = 'symbol';
+      }
+    })
   });
 };
 
